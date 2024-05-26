6d9baccd402b37db23d5c33e22327bb6366066c6
wiaamhilal <weaam224112@gmail.com>
2024-05-26 02:39:55 -0700

    all ready

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectToDb = require("./config/connectToDb");
+const {notFound, errorHandler} = require("./models/error");
 require("dotenv").config();
 
 const app = express();
@@ -13,6 +14,10 @@ app.use("/api/users", require("./routes/usersRoute"));
 app.use("/api/posts", require("./routes/postsRoute"));
 app.use("/api/comments", require("./routes/commentsRoute"));
 app.use("/api/category", require("./routes/categoryRoute"));
+
+// error handler middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
 
